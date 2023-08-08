c1bb9b968b4f8bc85d5332dbfc6ca1d25c3711fd
Arjun-cool <83638390+Arjun-cool@users.noreply.github.com>
2023-08-08 20:11:05 +0530

    Update ACON AIOT  TEMP REGULATOR.py

--- ACON AIOT  TEMP REGULATOR.py
+++ ACON AIOT  TEMP REGULATOR.py
@@ -1,6 +1,5 @@
 import requests                       #pip install requests
 
-# API key for OpenWeatherMap
 API_KEY = "56ac9609d73cd3406b671921ef9521c1"
 
 # The current location's latitude and longitude from the GPS sensor
@@ -23,7 +22,6 @@ diff = [(temp[0] - current_temp_accord) / 2, (hum[0] - current_hum_accord) / 2]
 temp_s = (temp[0] * 0.2 + temp[1] * 0.1 + current_temp_accord * 0.7) 
 hum_s = (hum[0] * 0.2 + hum[1] * 0.1 + current_hum_accord * 0.7)
 
-# Weather description from the API response
 main_des = api_data["current"]["weather"][0]["main"]
 
 # Set the ideal temperature and humidity based on the weather description
@@ -45,7 +43,7 @@ req_temp = (ideal_t * 3 + temp_s) /4
 req_hum = (ideal_h * 3 + hum_s) / 4
 
 print(f"Set temperature to {round(req_temp+diff[0])}°C")
-32
+
 # Determining the AC mode based on the required temperature and humidity
 req_hum +=diff[1]
 if (req_hum < hum_s):
